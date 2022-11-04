427c3a3f4e8902b95cca43fd06f1ddd91b783258
Lucas Souza <lucas.souza@eldorado.org.br>
2022-11-04 16:29:57 -0400

    add structuresclear

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,17 @@ import { AppComponent } from './app.component';
 import { PrivateRoutesComponent } from './shared/components/layouts/private-routes/private-routes.component';
 import { PublicRoutesComponent } from './shared/components/layouts/public-routes/public-routes.component';
 import { NotFoundComponent } from './shared/components/not-found/not-found.component';
+import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
+import { ToolbarComponent } from './shared/components/toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PrivateRoutesComponent,
     PublicRoutesComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    SidebarComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shared/components/sidebar/sidebar.data.ts
@@ -0,0 +1,27 @@
+export const menu = [
+  {
+    name: 'Home',
+    icon: 'home',
+    link: '',
+  },
+  {
+    name: 'Maps',
+    icon: 'location_on',
+    link: '',
+  },
+  {
+    name: 'Settings',
+    icon: 'settings',
+    link: '',
+  },
+  {
+    name: 'Accounts',
+    icon: 'person',
+    link: '',
+  },
+  {
+    name: 'Security',
+    icon: 'lock',
+    link: '',
+  },
+];
